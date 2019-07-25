5c2b3c17ec796da42c4aff7932062f335a878b0e
juliojesse25 <juliojesse25@gmail.com>
2019-07-24 23:49:51 -0400

    Add cyclicRotation.js file

---
+++ codewars-solutions/cyclicRotation.js
@@ -0,0 +1,18 @@
+function solution (A, K) {
+  let i = 1
+  while (i <= K) {
+    A.unshift(A.pop())
+    i++
+  }
+  return A
+}
+
+export default function solution2 (A, K) {
+  for(let i=1; i++; i<=K) {
+        
+        
+        
+    }
+}
+
+module.exports = solution
